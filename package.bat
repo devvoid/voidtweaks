05c04b6e4055d96e9e8f829ab86c87961319997d
Void <theprismaticvoid@gmail.com>
2022-03-22 17:32:14 -0500

    Bump pack format

---
+++ package.bat
@@ -0,0 +1,20 @@
+cd Convenience
+7z a -tzip ../Work/Convenience.zip * ../LICENSE
+
+cd ../ModifiedDrops
+7z a -tzip ../Work/ModifiedDrops.zip * ../LICENSE
+
+cd ../Redying
+7z a -tzip ../Work/Redying.zip * ../LICENSE
+
+cd ../Uncraftables
+7z a -tzip ../Work/Uncraftables.zip * ../LICENSE
+
+cd ../UnlockAll
+7z a -tzip ../Work/UnlockAll.zip * ../LICENSE
+
+cd ../NoPhantoms
+7z a -tzip ../Work/NoPhantoms.zip * ../LICENSE
+
+cd ../Cleanup
+7z a -tzip ../Work/Cleanup.zip * ../LICENSE
